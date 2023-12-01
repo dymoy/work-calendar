6b4effc8d1527227f899025c2e2d51b6428f9e07
Derek Moy <derekymoy@gmail.com>
2023-11-30 18:52:21 -0800

    WIP: color coding time blocks based on past, present, and future.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var root = $('#root');
+var currentDay = dayjs('2023-11-30 13:23');
+var timeBlocksContainer = $('#time-blocks-container');
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,11 +18,13 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  var currentTime = currentDay.hour();
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  $(`#currentDay`).text(dayjs().format('MMMM DD, YYYY'));
+  // $(`#currentDay`).text(dayjs().format('dddd, MMMM DD | HH:mm'));
+  $('#currentDay').text(currentDay.format('MMM D, YYYY | HH:mm'));
 });
